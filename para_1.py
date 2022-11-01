6d02a4c042a525a4601e79c6cfdbd007373e1820
Rohit Verma <verma-rohit@hotmail.co.uk>
2022-11-01 22:31:48 +0530

    Code to push config for multiple boxes

---
+++ para_1.py
@@ -0,0 +1,39 @@
+from http import client
+from  paramiko import client
+from  getpass import getpass 
+import time
+
+
+
+def cisco_cmd_execution(hostname,commands):
+    ssh_client = client.SSHClient()
+    ssh_client.set_missing_host_key_policy(client.AutoAddPolicy)
+    ssh_client.connect(hostname = hostname,
+                        port = 22,
+                        username = username,
+                        password = password,
+                        look_for_keys=False,
+                        )
+    print("Connected Sucessfully")
+
+    # Execute a command 
+    device_access = ssh_client.invoke_shell()
+    device_access.send("terminal len 0\n")
+    for cmd in commands:
+        device_access.send(f"{cmd}\n")
+        time.sleep(1)
+        output = device_access.recv(65535)
+        print(output.decode(),end="")
+
+    device_access.send("show run int loop1000\n")
+    time.sleep(1)
+    output = device_access.recv(65535)
+    print(output.decode(),end="")
+    ssh_client.close()
+
+hostnames = ["192.168.1.11","192.168.1.12"]
+for hostname in hostnames:
+    username = input(f"Please enter username for {hostname}? ")
+    password = getpass(f"\U0001F511 Enter password of user {username}: ") or "cisco"
+    commands = ["config t"," int loop 1000","ip address 100.100.100.100 255.255.255.255","end"]
+    cisco_cmd_execution(hostname,commands)
