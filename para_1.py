081bb857043121435762c7e59fc4635393d6e06c
Rohit Verma <verma-rohit@hotmail.co.uk>
2022-11-01 22:51:12 +0530

    Added Exceptional Handling

--- para_1.py
+++ para_1.py
@@ -1,37 +1,44 @@
 from http import client
-from  paramiko import client
+from  paramiko import client,ssh_exception
 from  getpass import getpass 
 import time
+import sys
 
 
 
 def cisco_cmd_execution(hostname,commands):
-    ssh_client = client.SSHClient()
-    ssh_client.set_missing_host_key_policy(client.AutoAddPolicy)
-    ssh_client.connect(hostname = hostname,
-                        port = 22,
-                        username = username,
-                        password = password,
-                        look_for_keys=False,
-                        )
-    print("Connected Sucessfully")
+    try:
+        ssh_client = client.SSHClient()
+        ssh_client.set_missing_host_key_policy(client.AutoAddPolicy)
+        ssh_client.connect(hostname = hostname,
+                            port = 22,
+                            username = username,
+                            password = password,
+                            look_for_keys=False,
+                            )
+        print("Connected Sucessfully")
 
-    # Execute a command 
-    device_access = ssh_client.invoke_shell()
-    device_access.send("terminal len 0\n")
-    for cmd in commands:
-        device_access.send(f"{cmd}\n")
+        # Execute a command 
+        device_access = ssh_client.invoke_shell()
+        device_access.send("terminal len 0\n") 
+        for cmd in commands:
+            device_access.send(f"{cmd}\n")
+            time.sleep(1)
+            output = device_access.recv(65535)
+            print(output.decode(),end="")
+
+        device_access.send("show run int loop1000\n")
         time.sleep(1)
         output = device_access.recv(65535)
         print(output.decode(),end="")
+        ssh_client.close()
+    except ssh_exception.AuthenticationException:
+        print(" Autentication Failed ")
+    except ssh_exception.NoValidConnectionsError:
+        print("Check hostname IP address")
+        
 
-    device_access.send("show run int loop1000\n")
-    time.sleep(1)
-    output = device_access.recv(65535)
-    print(output.decode(),end="")
-    ssh_client.close()
-
-hostnames = ["192.168.1.11","192.168.1.12"]
+hostnames = ["192.168.1.111","192.168.1.12"]
 for hostname in hostnames:
     username = input(f"Please enter username for {hostname}? ")
     password = getpass(f"\U0001F511 Enter password of user {username}: ") or "cisco"
