bf7e85510101ed28da13713a6460af0ac6fb293f
Rohit Verma <verma-rohit@hotmail.co.uk>
2022-11-03 19:23:23 +0530

    read and write into a file

--- file_operations.py
+++ file_operations.py
@@ -1,5 +1,6 @@
 from  paramiko import client,ssh_exception
 from  getpass import getpass 
+import datetime
 import time
 import sys
 import os
@@ -15,28 +16,41 @@ def cisco_cmd_push(hostname,commands):
                             look_for_keys=False)
 
         print("Connection established successfully")
+
+        # now = datetime.datetime.now().replace(microsecond=0)
+        file_name = f"{hostname}.txt"
+        print(file_name)
         device_access = device_ssh.invoke_shell()
         device_access.send("terminal length 0\n")
+        # Push config one cmd at a time
         for cmd in commands:
-            device_access.send(f"{cmd}\n")
+            device_access.send(f"{cmd}")
             time.sleep(1)
             outout = device_access.recv(65535)
             print(outout.decode(),end = " ")
-        
-        device_access.send("show run int loop 99\n")
-        time.sleep(1)
-        output = device_access.recv(65535)
-        print(output.decode(),end = "")
-        device_ssh.close()
+        # Push show run in a text file
+        with open(file_name,"w") as f:
+            device_access.send("show run\n")
+            time.sleep(10)
+            output = device_access.recv(65535)
+            f.write(output.decode())
+            device_ssh.close()
+    # Handling Authentication an wrong hostnames exceptions
     except ssh_exception.AuthenticationException:
         print("Correct the password")
     except ssh_exception.NoValidConnectionsError:
         print("Hostname IP is Incorrect")
     
 
+
+
+# Read from a file al the commands that need to be pushed
+with open("config.txt","r") as cfg:
+    config = cfg.readlines()
 hostnames = ["192.168.1.11","192.168.1.12"]
 for hostname in hostnames:
     username = input("Please enter an username ? ")
     password = getpass(f"Please enter password for {username} ?  ")
-    commands = ["config t","int loop 99","ip address 99.99.99.99 255.255.255.255","end"]
-    cisco_cmd_push(hostname,commands)
+    # commands = ["config t","int loop 99","ip address 99.99.99.99 255.255.255.255","end"]
+    cisco_cmd_push(hostname,config)
+ 
