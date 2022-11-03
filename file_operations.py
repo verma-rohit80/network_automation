ae00fbdd08d685453f0904a81821c2520d2ca0b9
Rohit Verma <verma-rohit@hotmail.co.uk>
2022-11-03 18:29:26 +0530

    Added Exception handling

--- file_operations.py
+++ file_operations.py
@@ -5,28 +5,33 @@ import sys
 import os
 
 def cisco_cmd_push(hostname,commands):
-    device_ssh = client.SSHClient()
-    device_ssh.set_missing_host_key_policy(client.AutoAddPolicy)
-    device_ssh.connect(hostname=hostname,
-                        port = 22,
-                        username = username,
-                        password = password,
-                        look_for_keys=False)
+    try:
+        device_ssh = client.SSHClient()
+        device_ssh.set_missing_host_key_policy(client.AutoAddPolicy)
+        device_ssh.connect(hostname=hostname,
+                            port = 22,
+                            username = username,
+                            password = password,
+                            look_for_keys=False)
 
-    print("Connection established successfully")
-    device_access = device_ssh.invoke_shell()
-    device_access.send("terminal length 0\n")
-    for cmd in commands:
-        device_access.send(f"{cmd}\n")
+        print("Connection established successfully")
+        device_access = device_ssh.invoke_shell()
+        device_access.send("terminal length 0\n")
+        for cmd in commands:
+            device_access.send(f"{cmd}\n")
+            time.sleep(1)
+            outout = device_access.recv(65535)
+            print(outout.decode(),end = " ")
+        
+        device_access.send("show run int loop 99\n")
         time.sleep(1)
-        outout = device_access.recv(65535)
-        print(outout.decode(),end = " ")
-    
-    device_access.send("show run int loop 99\n")
-    time.sleep(1)
-    output = device_access.recv(65535)
-    print(output.decode(),end = "")
-    device_ssh.close()
+        output = device_access.recv(65535)
+        print(output.decode(),end = "")
+        device_ssh.close()
+    except ssh_exception.AuthenticationException:
+        print("Correct the password")
+    except ssh_exception.NoValidConnectionsError:
+        print("Hostname IP is Incorrect")
     
 
 hostnames = ["192.168.1.11","192.168.1.12"]
