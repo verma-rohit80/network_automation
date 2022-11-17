637fd9a051228c972cabe552ded670fd9710d750
Rohit Verma <verma-rohit@hotmail.co.uk>
2022-11-17 13:47:27 +0530

    Enchanced Multithreading using concurrent

--- THREADING/1.config_puller.py
+++ THREADING/1.config_puller.py
@@ -28,22 +28,16 @@ devices = [r1,r2,r3,r4,r5]
 os.chdir("/home/rohit/Documents/python/Udemy/para_miko/para_virtual/THREADING")
 
 # Create a object and write into a file
-class_start = time.perf_counter()
+
 for device in devices:
-       router = CiscoConfigPull(**device)
-       hostname = router.session_connect()
        with cf.ThreadPoolExecutor() as executor:
-              output = [executor.submit(router.config_pull_run)]
-              for read_output in cf.as_completed(output):
-                     file_name = f"{hostname}__show_run.txt"
+              out = [executor.submit(CiscoConfigPull,**device)]
+              for return_output in cf.as_completed(out):
+                     file_name = f"{return_output.result().hostname}__show_run.txt"
                      with open(file_name,"w") as f:
-                            f.write(read_output.result())
-       router.session_close()
-class_end = time.perf_counter()
-print(f"Time took by class is {class_end-class_start}") 
+                            f.write(return_output.result().output)
 
-
-
+               
 
 
 
--- THREADING/conf/main_class.py
+++ THREADING/conf/main_class.py
@@ -8,16 +8,18 @@ class CiscoConfigPull:
         self.username = username
         self.password = password
         self.port = 22
+        self.session_connect()
 
     def session_connect(self):
         try:
-            self.session = client.SSHClient()
-            self.session.set_missing_host_key_policy(client.AutoAddPolicy)
-            self.session.connect(hostname=self.hostname,port=self.port,
-                            username=self.username,password=self.password,
-                            look_for_keys=False,allow_agent=False)
-            print("Connection established")
-            return self.hostname
+            with client.SSHClient() as self.session:
+                self.session.set_missing_host_key_policy(client.AutoAddPolicy)
+                self.session.connect(hostname=self.hostname,port=self.port,
+                                username=self.username,password=self.password,
+                                look_for_keys=False,allow_agent=False)
+                print("Connection established")
+                self.config_pull_run()
+
         except ssh_exception.AuthenticationException:
             print("Password is wrong")
         except ssh_exception.NoValidConnectionsError:
@@ -27,10 +29,8 @@ class CiscoConfigPull:
         self.object_access = self.session.invoke_shell()
         self.object_access.send("terminal length 0 \n")
         self.object_access.send("show run\n")
-        time.sleep(3)
-        output = self.object_access.recv(65535).decode()
-        return output
+        time.sleep(2)
+        self.output = self.object_access.recv(65535).decode()
+        return self.output,self.hostname
 
-    def session_close(self):
-        self.object_access.close()
-        print(f"{self.hostname} Connection is Closed")
+
